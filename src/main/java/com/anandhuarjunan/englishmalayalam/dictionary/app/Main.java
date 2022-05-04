ebafd08ecb6fc659f0e2fd1533bc78e347356d62
Anandhu Arjunan <anandhuka97@gmail.com>
2022-05-05 00:24:52 +0530

    changes

--- src/main/java/com/anandhuarjunan/englishmalayalam/dictionary/app/Main.java
+++ src/main/java/com/anandhuarjunan/englishmalayalam/dictionary/app/Main.java
@@ -1,7 +1,13 @@
 package com.anandhuarjunan.englishmalayalam.dictionary.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -9,8 +15,13 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/main.fxml")));
+
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,10 @@
 module com.anandhuarjunan.englishmalayalam.dictionary.app {
     requires javafx.controls;
     requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires com.jfoenix;
+    opens com.anandhuarjunan.englishmalayalam.dictionary.app.controller to javafx.fxml;
+
     exports com.anandhuarjunan.englishmalayalam.dictionary.app;
 }
